e343b2b9ad7f179abc9bd66e9f0cf6d36346d2e9
Amit Chotiya <v-amitch@microsoft.com>
2018-06-15 16:10:31 -0700

    Removed unnecessay comment

--- Binary Tree/Conclusion/SerializeAndDeserializeBinaryTree.cs
+++ Binary Tree/Conclusion/SerializeAndDeserializeBinaryTree.cs
@@ -11,15 +11,6 @@
  *     public TreeNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
 using System.Collections.Generic;
 
 public class Codec {
