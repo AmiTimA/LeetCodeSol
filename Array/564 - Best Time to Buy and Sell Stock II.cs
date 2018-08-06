c22beedc20494408531d9ef00384ef3f4ec5c843
Amit Chotiya <amit.chotiya@gmail.com>
2018-08-06 15:59:36 +0530

    564 - Best Time to Buy and Sell Stock II

---
+++ Array/564 - Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        // Base Case
+        if(prices == null || prices.Length <= 1) {
+            return 0;
+        }
+        
+        int min = 0;
+        int max = 0;
+        int result = 0;
+        int index = 0;
+        
+        
+        while(index < prices.Length - 1) {
+            
+            // Find local minimum
+            while(index < prices.Length - 1 && prices[index] >= prices[index + 1]){
+                index++;
+            }
+            
+            // Break if we have reached end
+            if(index == prices.Length - 1) {
+                return result;
+            }
+            
+            // Save min value
+            min = index++;
+            
+            // Find local maximum
+            while(index < prices.Length && prices[index] >= prices[index - 1]) {
+                index++;
+            }
+            
+            max = index - 1;
+            
+            result = result + prices[max] - prices[min];
+        }
+        
+        return result;
+    }
+}
