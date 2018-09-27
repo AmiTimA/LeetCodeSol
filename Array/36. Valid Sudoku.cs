1bb9a5df2133ca971c372fe701147ca57373928f
Amit Chotiya <v-amitch@microsoft.com>
2018-09-27 19:40:19 +0530

    Valid Sudoku and Rotate Images

---
+++ Array/36. Valid Sudoku.cs
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/valid-sudoku/description/
+
+public class Solution {
+    public bool IsValidSudoku(char[,] board) {
+        // Base Case
+        if (board == null || board.GetLength(0) != 9 || board.GetLength(1) != 9) {
+            return false;
+        }
+        
+        // Check each row
+        for (int i = 0; i < 9; i++) {
+            bool[] m = new bool[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i, j] != '.') {
+                    if (m[(int)(board[i, j] - '1')]) {
+                        return false;
+                    }
+                    m[(int)(board[i, j] - '1')] = true;
+                }                
+            }
+        }
+        
+        // Check each column
+        for (int j = 0; j < 9; j++) {
+            bool[] m = new bool[9];
+            for (int i = 0; i < 9; i++) {
+                if (board[i, j] != '.') {
+                    if (m[(int)(board[i, j] - '1')]) {
+                        return false;
+                    }
+                    m[(int)(board[i, j] - '1')] = true;
+                }                
+            }
+        }
+        
+        // Check each 3*3 matrix block
+        for (int block = 0; block < 9; block++) {
+            bool[] m = new bool[9];
+            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
+                for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
+                    if (board[i, j] != '.') {
+                        if (m[(int)(board[i, j] - '1')]) {
+                            return false;
+                        }
+                        m[(int)(board[i, j] - '1')] = true;
+                    }
+                }                
+            }
+        }
+        
+        return true;        
+    }
+}
---
+++ Array/48. Rotate Image.cs
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/rotate-image/
+
+// You are given an n x n 2D matrix representing an image.
+// Rotate the image by 90 degrees (clockwise).
+
+// Method 1
+public class Solution {
+    public void Rotate(int[,] matrix) { // two-dimensional array
+
+        // Base Case
+        if (matrix == null || matrix.GetLength(0) < 2) {
+            return;
+        }
+
+        // Find how many layers
+        int size = matrix.GetLength(0);
+        int layers = size / 2;           
+
+        for (int i = 0; i < layers; i++) {
+            for (int j = i; j < size - i - 1; j++) {
+                int temp = matrix[i, j];
+
+                matrix[i, j] = matrix[size - j - 1, i];
+
+                matrix[size - j - 1, i] = matrix[size - i - 1, size - j - 1];
+
+                matrix[size - i - 1, size - j - 1] = matrix[j, size - i - 1];
+
+                matrix[j, size - i - 1] = temp;
+            }
+        }        
+    }
+} // Complexity => O(n.n)
+
+// Method 2
+// First reverse up to down, then swap the symmetry 
+// https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
+
+public class Solution {
+    public void Rotate(int[,] matrix) {
+        // Base Case
+        if (matrix == null || matrix.GetLength(0) < 2) {
+            return;
+        }
+
+        int size = matrix.GetLength(0);
+                
+        // First upside down matrix
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = 0; j < size; j++) {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[size - i - 1, j];
+                matrix[size - i - 1, j] = temp;
+            }
+        }        
+        
+        // Reverse symmetry -> Convert Rows to Columns and Columsn to Rows
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                if(i != j) {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temp;
+                }
+            }
+        }
+    }
+} // Complexity => O(n.n)
